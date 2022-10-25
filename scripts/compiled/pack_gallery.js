ee4466373820a2f3ae0f46438cebce807257b0e6
pictimecloudaf-a <111168243+pictimecloudaf-a@users.noreply.github.com>
2022-10-25 15:37:56 -0400

    Update pack_gallery.js

--- scripts/compiled/pack_gallery.js
+++ scripts/compiled/pack_gallery.js
@@ -5,13 +5,13 @@ if (!window.rjsSessionId) {
 
 // Setup the functions
 if (!window.remoteSetupComplete) {
-  function uuidv4() {
+  window.uuidv4 = () => {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   }
 
-  function sendData(type, data) {
+  window.sendData = (type, data) => {
     try {
       const envelope = {};
       envelope.sessionId = window.rjsSessionId;
