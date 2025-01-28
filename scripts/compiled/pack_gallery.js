5895a80437c6f17d5234fba501488b702c49fe34
username <user@email>
2025-01-28 03:37:06 -0500

    fix

--- scripts/compiled/pack_gallery.js
+++ scripts/compiled/pack_gallery.js
@@ -2718,7 +2718,7 @@ if (!window.remoteSetupComplete) {
             'pictimeGUser': pictimeGUserToken,
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
           },
-          body: JSON.stringify({})
+          body: JSON.stringify({pageProjectId})
         }).then(resp => resp.json());
 
         const batch = loadProjectsBatchJson.d;
